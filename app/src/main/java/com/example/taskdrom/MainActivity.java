a16a8a6844df0de6baabbb389e8e77ecf9a8e01c
DmitryDG <runox@bk.ru>
2021-03-22 13:28:29 +1000

    Новые запросы

--- app/src/main/java/com/example/taskdrom/MainActivity.java
+++ app/src/main/java/com/example/taskdrom/MainActivity.java
@@ -2,28 +2,50 @@ package com.example.taskdrom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 import com.example.taskdrom.api.GitRequest;
 import com.example.taskdrom.model.GitRepo;
+import com.example.taskdrom.model.Issue;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
     private TextView textView;
+    private ImageView imageView;
     private int page;
     private String str = "";
 
 
+    private String testResponse;
+    private OkHttpClient client = new OkHttpClient().newBuilder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +54,47 @@ public class MainActivity extends AppCompatActivity {
 
         editText = (EditText) findViewById(R.id.editTextTextPersonName);
         textView = (TextView) findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
     }
 
 
+    @SuppressLint("SetTextI18n")
     public void push(View view) {
+
+        Thread thread = new Thread(() -> {
+            try {
+                HttpUrl.Builder httpBuilder = HttpUrl.parse("https://api.github.com/repos/android/testing-samples/issues").newBuilder();
+                Request request = new Request.Builder().url(httpBuilder.build()).build();
+                Response response = client.newCall(request).execute();
+                testResponse = response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray object;
+        try {
+            object = new JSONArray(testResponse);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //object.length();
+
+        textView.setText("");
+    }
+
+    public void push() {
         Map<String, String> params = new HashMap<>();
-        params.put(GitRequest.PARAM_QUERY, "test in:name");
+        params.put(GitRequest.PARAM_QUERY, "testing-sample in:name");
         params.put(GitRequest.PARAM_PER_PAGE, "5");
         params.put(GitRequest.PARAM_PAGE, "1");
 
@@ -45,15 +102,19 @@ public class MainActivity extends AppCompatActivity {
         GitRequest gitRequest = new GitRequest();
         str = gitRequest.get(GitRequest.SEARCH_REPO, params);
         List<GitRepo> repos = gitRequest.repoFromJSON(str);
-
         str = "";
         for(GitRepo repo : repos) {
+
             str += repo.getName() + "\n" + repo.getOwner() + "\n" + repo.getDesc() + "\n\n";
         }
+
+        imageView.setImageBitmap(repos.get(0).getAvatar());
+//        push();
+
 
         textView.setText(str);
 
     }
 
-
+//https://avatars.githubusercontent.com/u/32689599?v=4
 }
--- app/src/main/java/com/example/taskdrom/api/GitRequest.java
+++ app/src/main/java/com/example/taskdrom/api/GitRequest.java
@@ -1,7 +1,12 @@
 package com.example.taskdrom.api;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
 import com.example.taskdrom.model.GitRepo;
+import com.example.taskdrom.model.Issue;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,14 +28,19 @@ public class GitRequest {
     private String responseString;
     private Request request;
 
+    private Request reqAvatar;
+    private Response responseAvatar;
+
+    private Request reqIssues;
+
     private static final OkHttpClient client = new OkHttpClient().newBuilder()
             .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(3, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
             .build();
 
 
     //https://api.github.com/search/repositories?q=aserf%20in:name&per_page=50&page=1
-
+    public final static String FOR_ISSUES = "https://api.github.com/repos/";
     public final static String SEARCH_REPO = "https://api.github.com/search/repositories";
     public final static String PARAM_QUERY = "q";
     //этот параметр идет в q
@@ -83,8 +93,14 @@ public class GitRequest {
                 String desc = obj.getString("description");
                 if(desc.length() > 100)
                     desc = desc.substring(0, 100) + "...";
+                String url_avatar = obj.getJSONObject("owner").getString("avatar_url");
+                String url_issues = FOR_ISSUES + owner + "/" + name + "/issues";
 
-                GitRepo repo = new GitRepo(name, owner, desc);
+                Bitmap bitmap = getAvatar(url_avatar);
+                List<Issue> issues = getListIssue(url_issues);
+
+                GitRepo repo = new GitRepo(name, owner, desc, bitmap, issues);
+
                 list.add(repo);
             }
 
@@ -97,4 +113,59 @@ public class GitRequest {
     }
 
 
+    private Bitmap getAvatar(String url) {
+        reqAvatar = new Request.Builder().url(url).build();
+
+        Thread thread = new Thread(() -> {
+            try {
+                responseAvatar = client.newCall(reqAvatar).execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap mIcon11 = null;
+        if (responseAvatar.isSuccessful()) {
+            try {
+                mIcon11 = BitmapFactory.decodeStream(responseAvatar.body().byteStream());
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+
+        return mIcon11;
+    }
+
+    public List<Issue> getListIssue(String url) {
+        String json = get(url, null);
+
+        json = "{\"items\":" + json + "}";
+
+        List<Issue> list = new ArrayList<>();
+
+        try {
+            JSONObject obj = new JSONObject(json);
+            JSONArray items = obj.getJSONArray("items");
+            int x = 30;
+            if(items.length() < x) {
+                x = items.length();
+            }
+
+            for(int i = 0; i < x; i++) {
+                JSONObject it = items.getJSONObject(i);
+                String title = it.getString("title");
+                String login = it.getJSONObject("owner").getString("login");
+
+                list.add(new Issue(title, login));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
 }
